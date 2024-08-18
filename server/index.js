5c265982f9ba899b788c8620598f628ee834a30e
Trishit Hazra <hazratrishit@gmail.com>
2024-08-18 22:48:46 +0530

    Imported the connectDB function and connected to database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 
+// Import the connection function to connect to database
+const connectDB = require('./db');
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+// Connect to database
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
